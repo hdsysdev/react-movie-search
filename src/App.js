cc900771c3e71dba2c065e26178aa408424f5fab
Hubert <hubert.dudowicz@civicolive.com>
2021-08-30 12:08:21 +0100

    Fixed api query issue. Changed to using class component for page to better manage state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import { Container } from '@material-ui/core';
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import AppToolbar from './AppToolbar';
 import MovieItem from './MovieItem';
 
 const axios = require('axios');
 const apiUrl = 'http://www.omdbapi.com/?apikey=3c38531d&';
-function App() {
-    const [movies, setMovies] = useState([]);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {movies: []}
+    }
+    componentDidMount(){
 
-  axios.get(apiUrl + 's=Movie')
-      .then(res => {
-          const movieList = [];
-          res.data.Search.forEach(movie => {
-              movieList.push({id: movie.imdbID, title: movie.Title, description: movie.Type, poster: movie.Poster});
-          });
-          setMovies(movieList);
-          console.log(res);
-          // movies
-      })
-      .catch(error => {
-          console.log(error);
-      })
-  return (
-    <div className="App">
-      <AppToolbar/>
-      <Container maxWidth="lg">
-        {movies.map((mov) => <MovieItem movie={mov} key={mov.id}/>)}
-      </Container>
-    </div>
-  );
+        axios.get(apiUrl + 's=Movie&page=1')
+            .then(res => {
+                const movieList = [];
+                res.data.Search.forEach(movie => {
+                    movieList.push({id: movie.imdbID, title: movie.Title, description: movie.Type, poster: movie.Poster});
+                });
+                this.setState({movies: movieList});
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    render(){
+
+        return (
+            <div className="App">
+                <AppToolbar/>
+                <Container maxWidth="lg">
+                    {this.state.movies.map((mov) => <MovieItem movie={mov} key={mov.id}/>)}
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
